1521a06a8822dba9783bdb1baf9f6fa1a5d1efb0
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-08-16 17:34:22 +0530

    created an api for contact us form

--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Candidate = require("../modules/candidate");
+const Contact = require("../modules/contact");
 const router = express.Router();
 
 router.post("/api/application", async (req, res) => {
@@ -18,6 +19,27 @@ router.post("/api/application", async (req, res) => {
         })
 
         user = await candidate.save();
+        res.status(200).json({msg: "Succesfully submitted"})
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+router.post("/api/contact", async (req, res) => {
+    try {
+        
+        const {firstName, lastName, email, number, description} = req.body;
+
+        let contact = new Contact({
+            firstName,
+            lastName,
+            email,
+            number,
+            description,
+        })
+
+        user = await contact.save();
         res.status(200).json({msg: "Succesfully submitted"})
 
     } catch (error) {
